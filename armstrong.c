904c73389d01348754729657d3644e1e5f425b5a
Adhithyan K S <adhithyanktd@gmail.com>
2024-11-24 16:24:33 +0530

    armstrong edited

--- armstrong.c
+++ armstrong.c
@@ -1,26 +1,25 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+
 int main() {
-    int n,num,count,rem,sum=0;
-    printf("Enter a number to check if it is Armstrong:");
-    scanf("%d",&n);
-    count=0;
-    num=n;
-    while(num != 0) {
+    int n, num, count = 0, rem, sum = 0;
+    printf("Enter a number to check if it is Armstrong: ");
+    scanf("%d", &n);
+    num = n;
+    while (num != 0) {
         count++;
-        num=num/10;
+        num = num / 10;
     }
-    num=n;
-    while(num != 0) {
-        rem=num%10;
-        sum=sum+pow(rem,count);
-        num=num/10;
+    num = n;
+    while (num != 0) {
+        rem = num % 10;
+        sum += (int)pow(rem, count);
+        num = num / 10;
     }
-    if(n==sum) {
-        printf("%d is an Armstrong number.\n",n);
-    }
-    else {
-        printf("%d is not an Armstrong number.\n",n);
+    if (n == sum) {
+        printf("%d is an Armstrong number.\n", n);
+    } else {
+        printf("%d is not an Armstrong number.\n", n);
     }
     return 0;
 }
