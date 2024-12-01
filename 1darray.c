acabf3f445fb53b15e6e9b6d955ac82dfd1b183e
Adhithyan K S <adhithyanktd@gmail.com>
2024-12-01 16:45:08 +0530

    Initial version of candidates

--- 1darray.c
+++ 1darray.c
@@ -1,1 +1,10 @@
 #include<stdio.h>
+
+int main() {
+    int i;
+    char *candidate[6]={"NOTA","Abhinav","Ajay","Sooraj","Babil","John"};
+    printf("Candidate List:\n");
+    for(i=1;i<=5;i++) {
+        printf("%d. %s\n",i,candidate[i]);
+    }
+}
