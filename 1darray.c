ca69b92a6a7cfb7b98f9e4bd7a57cbf617743811
AdhithyanAsus <adhithyanktd@gmail.com>
2024-12-01 17:11:13 +0530

    1 D array completed

--- 1darray.c
+++ 1darray.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 
 int main() {
-    int i;
+    int i,option,vote,count[6]={0,0,0,0,0,0};
     char *candidate[6]={"NOTA","Abhinav","Ajay","Sooraj","Babil","John"};
     printf("Candidate List:\n");
     for(i=1;i<=5;i++) {
         printf("%d. %s\n",i,candidate[i]);
     }
+    do {
+        printf("Enter your vote:");
+        scanf("%d",&vote);
+        if(vote >=1 && vote <= 5) {
+            count[vote]++;
+        } else {
+            count[0]++;
+        }
+        printf("Is there any more voters?(0/1):");
+        scanf("%d",&option);
+    } while(option==1);
+    printf("\nResults:\n");
+    for(i=0;i<6;i++) {
+        printf("%3d %-20s %3d\n",i,candidate[i],count[i]);
+    }
+    return 0;
 }
