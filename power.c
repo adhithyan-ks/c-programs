46c3aa97ee76f260c721d58822c158525b90e0f7
Adhithyan K S <adhithyanktd@gmail.com>
2024-12-01 14:58:36 +0530

    Main added to calculate power

--- power.c
+++ power.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+int power(int b,int p);
 int main() {
-    
+    int b,p;
+    printf("Enter Base and Power:");
+    scanf("%d %d",&b,&p);
+    printf("Power is %d",power(b,p));
 }
