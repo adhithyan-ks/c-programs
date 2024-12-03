ca2f1ed8fc186c2f56da02e1f950a497dc86f142
AdhithyanAsus <adhithyanktd@gmail.com>
2024-12-03 23:15:27 +0530

    Binary Search completed

--- binarySearch.c
+++ binarySearch.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int binarySearch(int[],int,int);
+int binarySearch(int[],int,int,int);
 int main() {
     int i,size,arr[10],searchNum,result;
     printf("Enter size of array:");
@@ -10,11 +10,27 @@ int main() {
     }
     printf("Enter Number to Search:");
     scanf("%d",&searchNum);
-    result=binarySearch(arr,size,searchNum);
+    result=binarySearch(arr,0,size-1,searchNum);
     if(result==-1) {
         printf("Number was not found");
     }
     else {
         printf("Number found at index %d",result);
     }
+}
+int binarySearch(int arr[10],int lower,int upper,int searchNum) {
+    int mid;
+    while(lower<=upper) {
+        mid=(lower+upper)/2;
+        if(arr[mid]==searchNum) {
+            return(mid);
+        }
+        if(searchNum>arr[mid]) {
+            lower=mid+1;
+        }
+        else {
+            upper=mid-1;
+        }
+    }
+    return -1;
 }
