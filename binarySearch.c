6550f84464409dcf222e6efeb5918da1655d170e
AdhithyanAsus <adhithyanktd@gmail.com>
2024-12-03 22:33:30 +0530

    Binary Search initial

---
+++ binarySearch.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int i,size,arr[10];
+    printf("Enter size of array:");
+    scanf("%d",&size);
+    printf("Enter array elements:");
+    for(i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+}
