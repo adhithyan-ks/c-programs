b3dcbd36928f9ab5605e3fb887f2aa621c067686
AdhithyanAsus <adhithyanktd@gmail.com>
2024-12-03 11:48:51 +0530

    Linear search array reading

--- linearsearch.c
+++ linearsearch.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+
+int main() {
+    int size,i;
+    printf("Enter array size:");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter array elements:\n");
+    for(i=0;i<size;i++) {
+        scanf("%d",&arr[i]);
+    }
+}
